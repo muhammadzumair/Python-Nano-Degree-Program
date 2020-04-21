9b9e1328a15a05a65dfbec5ad609cdd2345ab685
muhammadzumair <zumairmuhammad@gmail.com>
2020-04-21 22:16:00 +0500

    date aggregation

--- SQL-Basic/sql-aggregation.sql
+++ SQL-Basic/sql-aggregation.sql
@@ -165,3 +165,176 @@ ORDER BY num_accounts;
 
 SELECT DISTINCT id, name
 FROM sales_reps;
+
+
+
+/*---------------------------HAVING-----------------------------*/
+
+/*How many of the sales reps have more than 5 accounts that they manage?*/
+SELECT s.id, s.name, COUNT(*) num_accounts
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+HAVING COUNT(*) > 5
+ORDER BY num_accounts;
+
+
+/*and technically, we can get this using a SUBQUERY as shown below. This same logic can be used for the other queries, but this will not be shown.*/
+SELECT COUNT(*) num_reps_above5
+FROM(SELECT s.id, s.name, COUNT(*) num_accounts
+     FROM accounts a
+     JOIN sales_reps s
+     ON s.id = a.sales_rep_id
+     GROUP BY s.id, s.name
+     HAVING COUNT(*) > 5
+     ORDER BY num_accounts) AS Table1;
+
+
+/*How many accounts have more than 20 orders?*/
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING COUNT(*) > 20
+ORDER BY num_orders;
+
+
+
+/*Which account has the most orders?*/
+SELECT a.id, a.name, COUNT(*) num_orders
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY num_orders DESC
+LIMIT 1;
+
+
+/*How many accounts spent more than 30,000 usd total across all orders?*/
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING SUM(o.total_amt_usd) > 30000
+ORDER BY total_spent;
+
+
+/*How many accounts spent less than 1,000 usd total across all orders?*/
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+HAVING SUM(o.total_amt_usd) < 1000
+ORDER BY total_spent;
+
+
+/*Which account has spent the most with us?*/
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_spent DESC
+LIMIT 1;
+
+
+/*Which account has spent the least with us?*/
+SELECT a.id, a.name, SUM(o.total_amt_usd) total_spent
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_spent
+LIMIT 1;
+
+
+/*Which accounts used facebook as a channel to contact customers more than 6 times?*/
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+HAVING COUNT(*) > 6 AND w.channel = 'facebook'
+ORDER BY use_of_channel;
+
+
+/*Which account used facebook most as a channel?*/
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+WHERE w.channel = 'facebook'
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 1;
+/*Note: This query above only works if there are no ties for the account that used facebook the most.
+ It is a best practice to use a larger limit number first such as 3 or 5 to see if there are ties before using LIMIT 1.*/
+
+
+
+/*Which channel was most frequently used by most accounts?*/
+SELECT a.id, a.name, w.channel, COUNT(*) use_of_channel
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id
+GROUP BY a.id, a.name, w.channel
+ORDER BY use_of_channel DESC
+LIMIT 10;
+
+
+
+/*--------------------DATES----------------------------*/
+
+/*Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least.
+ Do you notice any trends in the yearly sales totals?*/
+ SELECT DATE_PART('year', occurred_at) ord_year,  SUM(total_amt_usd) total_spent
+ FROM orders
+ GROUP BY 1
+ ORDER BY 2 DESC;
+/*When we look at the yearly totals, you might notice that 2013 and 2017 have much smaller totals than all other years.
+ If we look further at the monthly data, we see that for 2013 and 2017 there is only one month of sales for each of these years (12 for 2013 and 1 for 2017).
+  Therefore, neither of these are evenly represented. Sales have been increasing year over year, with 2016 being the largest sales to date.
+   At this rate, we might expect 2017 to have the largest sales.*/
+
+
+/*Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?*/
+/*In order for this to be 'fair', we should remove the sales from 2013 and 2017. For the same reasons as discussed above.*/
+SELECT DATE_PART('month', occurred_at) ord_month, SUM(total_amt_usd) total_spent
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC; 
+/*The greatest sales amounts occur in December (12).*/
+
+
+/*Which year did Parch & Posey have the greatest sales in terms of total number of orders? Are all years evenly represented by the dataset?*/
+SELECT DATE_PART('year', occurred_at) ord_year,  COUNT(*) total_sales
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC;
+/*Again, 2016 by far has the most amount of orders, but again 2013 and 2017 are not evenly represented to the other years in the dataset.*/
+
+
+/*Which month did Parch & Posey have the greatest sales in terms of total number of orders? Are all months evenly represented by the dataset?*/
+SELECT DATE_PART('month', occurred_at) ord_month, COUNT(*) total_sales
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC;
+/*December still has the most sales, but interestingly, November has the second most sales (but not the most dollar sales. To make a fair comparison from one month to another 2017 and 2013 data were removed.*/
+
+
+/*In which month of which year did Walmart spend the most on gloss paper in terms of dollars?*/
+SELECT DATE_TRUNC('month', o.occurred_at) ord_date, SUM(o.gloss_amt_usd) tot_spent
+FROM orders o 
+JOIN accounts a
+ON a.id = o.account_id
+WHERE a.name = 'Walmart'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+/*May 2016 was when Walmart spent the most on gloss paper.*/
